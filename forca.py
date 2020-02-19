f19065308bf3d6a8c493531f8329b62df3f394a3
Lucas Garcia <lucasugarcia@gmail.com>
2020-02-18 23:52:40 -0300

    Guardando as letras acertadas em uma lista

--- forca.py
+++ forca.py
@@ -4,11 +4,29 @@ def jogar():
     print("*********************************")
 
     palavra_secreta = "banana"
+    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
     enforcou = False
     acertou = False
 
-    while(not enforcou and not acertou):
+    print(letras_acertadas)
 
+    while(not enforcou and not acertou):
+        chute = input("Qual letra?")
+        chute = chute.strip()
+
+        if(len(chute) > 1):
+            print("Digite apenas uma letra!")
+            continue;
+
+        posicao = 0
+
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                letras_acertadas[posicao] = letra
+
+            posicao = posicao + 1
+
+        print(letras_acertadas)
 
     print("Fim do jogo")
 
